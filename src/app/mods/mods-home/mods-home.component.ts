d6dfdd0918e0502c238ccfbcfcf15169c8f192d3
sapinodejs@gmail.com <>
2020-03-06 13:33:36 +0100

    adding accordion

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ModsHomeComponent implements OnInit {
 
   modalOpen = false;
+  items = [
+    {title: 'was ist das?', content: 'das ist ein Accordion'},
+    {title: 'was magst du am liebsten?', content: 'Kochen und essen mache ich sehr gerne'},
+    {title: 'wie heiß die mutter von Nici Laudar?', content: 'Mama Laudar, Mama Laudar...'}
+  ]
 
   constructor() { }
 
